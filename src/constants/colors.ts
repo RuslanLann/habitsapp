0a34652c7ed4ffa00870f5e3ceaa5e89c17e4a3a
Ruslan Ramazanov <ruslanramazanov@Dev-MacBook-Pro.local>
2020-10-03 19:43:50 +0300

    radar-chart - color palette

---
+++ src/constants/colors.ts
@@ -0,0 +1,13 @@
+const RED = '#e76f51';
+const ORANGE = '#f4a261';
+const YELLOW = '#e9c46a';
+const GREEN = '#2a9d8f';
+const BLUE = '#264653';
+
+export default {
+  RED,
+  ORANGE,
+  YELLOW,
+  GREEN,
+  BLUE,
+};
