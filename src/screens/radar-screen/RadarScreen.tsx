acec9e7b0e56d56e879b9201802c73d684df81b6
Ruslan Ramazanov <ruslanramazanov@Dev-MacBook-Pro.local>
2020-10-09 00:51:50 +0300

    markup - new bg color of RadarScreen

--- src/screens/radar-screen/RadarScreen.tsx
+++ src/screens/radar-screen/RadarScreen.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
 
 import { AddDataButton, FeatureList, RadarChart } from '../../components';
-import { sizes } from '../../constants';
+import { colors, sizes } from '../../constants';
 
 const RadarScreen = () => (
   <SafeAreaView style={styles.safeAreaView}>
     <StatusBar barStyle="dark-content" />
     <View style={styles.screenContainer}>
-      <RadarChart />
+      <View style={styles.chartContainer}>
+        <RadarChart />
+      </View>
       <FeatureList />
       <AddDataButton />
     </View>
@@ -18,12 +20,17 @@ const RadarScreen = () => (
 const styles = StyleSheet.create({
   safeAreaView: {
     flex: 1,
+    backgroundColor: colors.BLACK,
   },
   screenContainer: {
     flexGrow: 1,
     padding: sizes.PADDING_HORIZONTAL,
     justifyContent: 'center',
     alignItems: 'center',
+    borderRadius: 20,
+  },
+  chartContainer: {
+    backgroundColor: colors.SECOND_BLACK,
   },
 });
 
