499069ba6a24ac342d3a196bec327f50e97ce6af
Ruslan Ramazanov <ruslanramazanov@Dev-MacBook-Pro.local>
2021-06-10 22:44:40 +0300

    markup - deps upgrade

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 
+console.logObject = (...args) => {
+  console.log(JSON.stringify(args[0], null, 4), ...args);
+};
+
 AppRegistry.registerComponent(appName, () => App);
