6acc68422a9c8a5504239a2dc157dad6168b4a4a
Ruslan Ramazanov <ruslanramazanov@Dev-MacBook-Pro.local>
2021-07-23 17:21:03 +0300

    markup - disabled Flipper, connected Reactotron

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+if (__DEV__) {
+  import('./reactotronConfig').then(() => console.log('Reactotron Configured'));
+  // NativeModules.DevMenu.show();
+}
+
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
